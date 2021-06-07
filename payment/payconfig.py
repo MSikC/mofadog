e36b61c93aaaa2f7838548e06be20c0502846d1f
MSikC <louisik@outlook.com>
2021-06-07 18:15:03 +0800

    delete secret info

--- payment/payconfig.py
+++ payment/payconfig.py
@@ -1,9 +1,9 @@
 #-*- coding:utf-8 -*-
 
-payId = 10990
+payId = 'XXX'
 #Int	是	9	商户ID	10000
-payKey = '14a5efb0332f26d4a68e4e00e0482eb1'
-Notify_url = 'http://www.mofadog.com/payment/notify/'
+payKey = 'XXX'
+Notify_url = 'XXX'
 #String	是	255	服务器异步通知	https://www.ispay.cn/notify/
-Return_url = 'http://www.mofadog.com/payment/return/'
+Return_url = 'XXX'
 #String	是	255	客户端同步跳转	https://www.ispay.cn/return/
